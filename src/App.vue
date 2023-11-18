fa566ce039b221ba8de0053ad9d6e93bb312deb3
siaoliu66 <siaoliu66@gmail.com>
2023-11-18 11:51:15 +0800

    完成pc版民眾服務

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,40 @@ import footermenu from './components/footer.vue'
     <div class="news" id="news"></div>
     <div class="policy" id="Policy"></div>
     <div class="donate" id="Donate"></div>
-    <div class="sevices" id="Sevices"></div>
+    <div class="sevices" id="Sevices">
+      <div class="div">
+        <div class="text">
+        <p class="title">
+        您的聲音, 我們的行動!
+      </p>
+      <p class="content">
+        親愛的鄉親，每一位市民的意見都是我們社區前進的原動力。無論大小事，我都誠摯希望聽到您的建議。分享您的想法，一同為我們的未來
+      <br/>打造更美好！
+      </p>
+      </div>
+      <div class="form">
+        <div class="form_wrap">
+          <label for="name">您的姓名</label>
+          <input type="text" name="name" id="name">
+        </div>  
+        <div class="form_wrap">
+          <label for="name">您的Email</label>
+          <input type="text" name="mail" id="mail" placeholder="Email及手機可擇一填寫">
+        </div>
+        <div class="form_wrap">
+          <label for="name">您的手機</label>
+          <input type="text" name="phone" id="phone" placeholder="Email及手機可擇一填寫">
+        </div>
+        <div class="form_wrap feedback">
+          <label for="name">您的建言</label>
+          <textarea name="feedback" id="feedback" cols="30" rows="10" placeholder="請填寫1000字內文字..."></textarea>
+        </div>  
+        <input type="submit" value="送出意見" class="send">
+      </div>
+      </div>
+      
+      
+    </div>
   </div>
   <footermenu />
 </template>
